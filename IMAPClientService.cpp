009719b67287172e42f6d854b2be15e53671ba4c
SHWETA NERAKE <100186120+csnerake@users.noreply.github.com>
2022-08-17 18:08:08 +0530

    Update IMAPClientService.cpp

--- IMAPClientService.cpp
+++ IMAPClientService.cpp
@@ -1,4 +1,16 @@
 #include "IMAPClientService.h"
+#include <cstdarg>
+ 
+int add(int first, int second, ...) {
+  int r = first + second; 
+  va_list va;
+  va_start(va, second);
+  while (int v = va_arg(va, int)) {
+    r += v;
+  }
+  va_end(va);
+  return r;
+}
 
 IMAPClientService::IMAPClientService(unsigned int nRecvSecTimeout) : ClientService(nRecvSecTimeout) { 
    this->nDefaultPort = IMAP_CLIENT_SERVICE_DEFAULT_PORT;
