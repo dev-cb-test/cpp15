792ae25e91634ef8fe42387c95455d9a47660255
SHWETA NERAKE <100186120+csnerake@users.noreply.github.com>
2022-08-17 18:26:05 +0530

    Update IMAPClientService.cpp

--- IMAPClientService.cpp
+++ IMAPClientService.cpp
@@ -1,5 +1,17 @@
 #include "IMAPClientService.h"
 #include <cstdarg>
+
+char npath[MAXPATHLEN];
+int i;
+
+for(i = 0 ; *name != '\0' && i < sizeof(npath) - 1 ; i ++, name++)
+{
+	npath[i] = *name;
+	if(*name == '"')
+		npath[++i] = '"';
+}
+npath[i] = '\0';
+
  
 int add(int first, int second, ...) {
   int r = first + second; 
